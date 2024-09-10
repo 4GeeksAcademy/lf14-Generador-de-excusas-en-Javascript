2465883b88a911b91d2bcbbf434cc2abaf96f350
lf14 <54648106+lf14@users.noreply.github.com>
2024-09-10 01:43:32 +0000

    [generador de excusas]
    -index.html se establece estructura, título, contenido de texto y botón.
    -app.js se establecen las opciones y la función para generar excusa asociada al botón.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //opciones de quien
+  const quien = [
+    "Mi aspiradora robot",
+    "Una falla en la matrix",
+    "Un ataque de amnesia colectiva",
+    "Una invasión alienígena",
+    "Un error en la simulación"
+  ];
+  //opciones de que hizo
+  const Hizo = [
+    "hizo desaparecer",
+    "eliminó en forma permanente",
+    "desactivó temporalmente",
+    "reprogramó",
+    "alteró"
+  ];
+  //opciones de lo afectado
+  const objetoAfectado = [
+    "la red",
+    "mi teléfono",
+    "mi código",
+    "mi computadora",
+    "mi memoria"
+  ];
+  //función que combina las const para generar la excusa
+  function generarExcusa() {
+    //math.random devuelve random y math.floor redondea
+    const randomquien = quien[Math.floor(Math.random() * quien.length)];
+    const randomHizo = Hizo[Math.floor(Math.random() * Hizo.length)];
+    const randomobjetoAfectado =
+      objetoAfectado[Math.floor(Math.random() * objetoAfectado.length)];
+    const excuse = `${randomquien} ${randomHizo} ${randomobjetoAfectado}.`;
+    document.getElementById("excuse").innerHTML = excuse;
+    const generarButton = document.getElementById("generar");
+    generarButton.addEventListener("click", generarExcusa);
+  }
+
+  generarExcusa();
 };
