cdacd91104d4f7531205fb1fa6efc35b733b3210
MansillaLorena <lorenamansillag@gmail.com>
2024-09-17 00:10:44 +0000

    funcion nueva

--- src/app.js
+++ src/app.js
@@ -4,21 +4,21 @@ import "./style.css";
 window.onload = function() {
   //opciones de quien
   const quien = [
-    "¡Mi aspiradora robot",
-    "¡Una falla en la matrix",
-    "¡Un ataque de amnesia colectiva",
-    "¡Una invasión alienígena",
-    "¡Un error en la simulación",
-    "¡Un duende eléctricista"
+    "¡Mi aspiradora robot ",
+    "¡Una falla en la matrix ",
+    "¡Un ataque de amnesia colectiva ",
+    "¡Una invasión alienígena ",
+    "¡Un error en la simulación ",
+    "¡Un duende eléctricista "
   ];
   //opciones de que hizo
   const Hizo = [
-    "hizo desaparecer",
-    "eliminó en forma permanente",
-    "desactivó temporalmente",
-    "reprogramó",
-    "alteró",
-    "desmaterializó"
+    "hizo desaparecer ",
+    "eliminó en forma permanente ",
+    "desactivó temporalmente ",
+    "reprogramó ",
+    "alteró ",
+    "desmaterializó "
   ];
   //opciones de lo afectado
   const objetoAfectado = [
@@ -29,14 +29,25 @@ window.onload = function() {
     "mi memoria!",
     "el tejido del espacio-tiempo!"
   ];
+  function generarAleatorio(array) {
+    return Math.floor(Math.random() * array.length);
+  }
   //función que combina las const para generar la excusa
   function generarExcusa() {
     //math.random devuelve random y math.floor redondea
-    const randomquien = quien[Math.floor(Math.random() * quien.length)];
-    const randomHizo = Hizo[Math.floor(Math.random() * Hizo.length)];
-    const randomobjetoAfectado =
-      objetoAfectado[Math.floor(Math.random() * objetoAfectado.length)];
-    const excuse = `${randomquien} ${randomHizo} ${randomobjetoAfectado}.`;
+    // const randomquien = quien[Math.floor(Math.random() * quien.length)]; //
+    //const randomHizo = Hizo[Math.floor(Math.random() * Hizo.length)]; //
+    //const randomobjetoAfectado =
+    //  objetoAfectado[Math.floor(Math.random() * objetoAfectado.length)]; ///
+    //const excuse = `${randomquien} ${randomHizo} ${randomobjetoAfectado}.`;
+
+    const excuse =
+      quien[generarAleatorio(quien)] +
+      " " +
+      Hizo[generarAleatorio(Hizo)] +
+      " " +
+      objetoAfectado[generarAleatorio(objetoAfectado)];
+
     document.getElementById("excuse").innerHTML = excuse;
     const generarButton = document.getElementById("generar");
     generarButton.addEventListener("click", generarExcusa);
